957ec728dd111fa254211367dd7abcd63ec5ced6
Julian Geiger <julian.geiger@psi.ch>
2025-03-13 14:38:47 +0100

    Push before GM

---
+++ aiida-wg-pythonjob-qe-to_universal.py
@@ -0,0 +1,233 @@
+# ! Specify Python Code manually to point to executable of my Python venv
+
+
+from pathlib import Path
+import subprocess
+from rich.pretty import pprint
+from turtle import st
+from typing import Any
+import shutil
+import os
+import ipdb
+from ase import Atoms
+from adis_tools.parsers import parse_pw
+from ase.io import write
+
+from aiida import load_profile, orm
+from aiida_workgraph import WorkGraph, task
+from atomistic_engine_unification.calculate_funcs_aiida_wg_pythonjob import (
+    get_bulk_structure,
+    calculate_qe,
+    generate_structures,
+    plot_energy_volume_curve,
+    all_scf
+)
+
+load_profile()
+
+pseudopotentials = {"Al": "Al.pbe-n-kjpaw_psl.1.0.0.UPF"}
+pseudo_path = Path(
+    "/home/geiger_j/aiida_projects/adis/git-repos/compare-workflow-graphs/pseudos"
+)
+# pseudo_path = pathlib.Path.cwd() / "pseudos"
+strain_lst = [0.9, 0.95, 1, 1.05, 1.10]
+# strain_lst = [0.9, 1]
+
+scf_input_dict = {
+    "pseudopotentials": pseudopotentials,
+    "kpts": (1, 1, 1),  # (3, 3, 3),
+    # "kpts": (3, 3, 3),
+    "calculation": "scf",  # "vc-relax",
+    # "calculation": "vc-relax",
+    "smearing": 0.02,
+}
+
+
+relax_input_dict = {
+    "pseudopotentials": pseudopotentials,
+    "kpts": (1, 1, 1),  # (3, 3, 3),
+    # "kpts": (3, 3, 3),
+    # "calculation": "scf",  # "vc-relax",
+    "calculation": "vc-relax",
+    "smearing": 0.02,
+}
+
+get_bulk_structure_dec = task.pythonjob(outputs=[{"name": "structure"}])(
+    get_bulk_structure
+)
+
+generate_structures_dec = task.pythonjob(
+    outputs=[
+        {
+            "name": "scaled_atoms",
+            "identifier": "workgraph.namespace",
+            "metadata": {"dynamic": True},
+        }
+    ]
+)(generate_structures)
+
+calculate_qe_dec = task.pythonjob(
+    outputs=[
+        {"name": "structure"},
+        {"name": "energy"},
+        {"name": "volume"},
+    ]
+)(calculate_qe)
+
+# all_scf_dec = task.pythonjob(
+#     outputs=[
+#         {
+#             "name": "qe_results",
+#             # "identifier": "workgraph.namespace",
+#             # "metadata": {"dynamic": True},
+#         }
+#     ]
+# )(all_scf)
+
+plot_energy_volume_curve_dec = task.pythonjob()(plot_energy_volume_curve)
+
+
+wg = WorkGraph("wg-pythonjob")
+
+get_bulk_structure_task = wg.add_task(
+    get_bulk_structure_dec,
+    name="get_bulk_structure",
+    element="Al",
+    a=4.05,
+    cubic=True,
+)
+
+relax_task = wg.add_task(
+    calculate_qe_dec,
+    name="relax",
+    structure=get_bulk_structure_task.outputs.structure,
+    input_dict=relax_input_dict,
+    working_directory="relax",
+)
+
+generate_structures_task = wg.add_task(
+    generate_structures_dec,
+    name="generate_structures",
+    structure=relax_task.outputs.structure,
+    strain_lst=strain_lst,
+)
+
+# wg.run()
+
+ipdb.set_trace()
+
+for istrain, strain in enumerate(strain_lst):
+
+    task_name = f"scf_{istrain}"
+    key = f"s_{istrain}"
+
+    scf_task = wg.add_task(
+        calculate_qe_dec,
+        name=task_name,
+        # Possibly create sockets on access for dynamic namespaces ???
+        # structure=generate_structures_task.outputs.scaled_atoms[istrain],
+        # structure=generate_structures_task.outputs.scaled_atoms.get(key),
+
+        # _Some_ syntax that says get element x, even before execution
+        input_dict=scf_input_dict,
+        working_directory=task_name,
+    )
+
+# all_scf_task = wg.add_task(
+#     all_scf_dec,
+#     name='all_scf',
+#     structures=generate_structures_task.outputs.scaled_atoms,
+#     input_dict=scf_input_dict
+# )
+
+# plot_energy_volume_curve_task = wg.add_task(
+#     plot_energy_volume_curve_dec,
+#     name='plot_energy_volume_curve',
+#     qe_results=all_scf_task.outputs.qe_results,
+# )
+
+wg.to_html()
+wg_dict = wg.to_dict()
+# wg.run()
+
+
+# import ipdb; ipdb.set_trace()
+
+# wg.run()
+
+
+# ! This seems eerily similar to the `NodeLink` class from node-graph
+def get_edges_list(wg_dict):
+
+    edges_label_lst = []
+    for link_dict in wg_dict["links"]:
+        if link_dict["from_socket"] == "result":
+            edges_label_lst.append(
+                {
+                    "target": link_dict["to_node"],
+                    "targetHandle": link_dict["to_socket"],
+                    "source": link_dict["from_node"],
+                    "sourceHandle": None,
+                }
+            )
+        else:
+            edges_label_lst.append(
+                {
+                    "target": link_dict["to_node"],
+                    "targetHandle": link_dict["to_socket"],
+                    "source": link_dict["from_node"],
+                    "sourceHandle": link_dict["from_socket"],
+                }
+            )
+
+    return edges_label_lst
+
+
+kwargs_dict, function_dict = {}, {}
+
+from node_graph.executor import NodeExecutor
+
+for task_name, task_dict in wg_dict["tasks"].items():
+
+    # Extract relevant input variable names (excluding metadata and _wait)
+    input_variables = []
+    for param in task_dict["inputs"]:
+        if not param.startswith("metadata") and not param.startswith("_wait") and not param.startswith('monitors'):
+            input_variables.append(param)
+
+    # Prepare input keyword arguments
+    input_kwargs = {}
+
+    for param in input_variables:
+        print(f"PARAM: {param}")
+        try:
+            property_value = task_dict["inputs"][param]["property"]["value"]
+        except:
+            # ipdb.set_trace()
+            # raise
+            pass
+
+        if isinstance(property_value, dict):
+            try:
+                input_kwargs[param] = property_value.value
+            except:
+                input_kwargs[param] = property_value
+                # ipdb.set_trace()
+        else:
+            input_kwargs[param] = property_value
+
+    # function_dict[task_name] = loads(task_dict['executor']['callable']).process_class._func
+    kwargs_dict[task_name] = input_kwargs
+    try:
+        executor = NodeExecutor(**task_dict['executor']).executor
+        function_dict[task_name] = executor
+    except:
+        ipdb.set_trace()
+
+    # function_dict[task_name] = task_dict["executor"]["callable"]
+
+
+print("KWARGS_DICT")
+pprint(kwargs_dict)
+print("FUNCTION_DICT")
+pprint(function_dict)
